4b9ba8f73aade3a760c9861c93998f05022d9962
jinghengkhoo <89514836+jinghengkhoo@users.noreply.github.com>
2024-07-11 23:18:55 +0800

    fix: unremoved debug files/code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,7 @@ import Profile from "./components/Profile";
 import HotelSearch from "./components/HotelSearch";
 import HotelDetails from "./components/HotelDetails";
 import BookingFormWrapper from "./components/BookingForm";
-<<<<<<< Updated upstream
 import AdminLogin from "./components/AdminLogin";
-=======
-import Hello from "./components/Hello";
->>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -24,11 +20,7 @@ const App = () => {
         <Route path="/hotels" element={<HotelSearch />} />
         <Route path="/hotels/:id" element={<HotelDetails />} />
         <Route path="/book/:id" element={<BookingFormWrapper />} />
-<<<<<<< Updated upstream
         <Route path="/admin" element={<AdminLogin />} />
-=======
-        <Route path="/hello" element={<Hello />} />
->>>>>>> Stashed changes
       </Routes>
     </AuthProvider>
   );
--- frontend/src/components/Hello.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Hello() {
-    return <div data-testid="login">Hello</div>;
-}
-
-export default Hello;
