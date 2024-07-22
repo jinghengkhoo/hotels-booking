8115c230c3370c7dcede12e2407c2353ac0648c2
Gregory <49094805+glimeuxe@users.noreply.github.com>
2024-07-23 03:26:06 +0800

    fix: added redirect http(80) to http(PORT)

--- backend/server.js
+++ backend/server.js
@@ -21,19 +21,38 @@ mongoose
 
 		if (httpsOptions.key && httpsOptions.cert) {
 			https.createServer(httpsOptions, app).listen(PORT, () => {
-				console.log(`App is listening on port: ${PORT}`);
+				console.log(`App is listening on port ${PORT} (HTTPS)`);
+			});
+
+			// Redirect from HTTP on port 80 to HTTPS on port PORT
+			http.createServer((req, res) => {
+				res.writeHead(301, { "Location": `https://localhost:${PORT}${req.url}` });
+				res.end();
+			}).listen(80, () => {
+				console.log("HTTP server listening on port 80");
+			});
+
+			// Redirect from HTTP on port PORT to HTTPS on port PORT is impossible using createServer as 1 port can have up to 1 server
+
+			// Redirect from HTTPS on port 443 to HTTPS on port PORT
+			https.createServer(httpsOptions, (req, res) => {
+				res.writeHead(301, { "Location": `https://localhost:${PORT}${req.url}` });
+				res.end();
+			}).listen(443, () => {
+				console.log("HTTPS server listening on port 443");
 			});
 		} else {
 			http.createServer(app).listen(PORT, () => {
-				console.log(`App is listening on port: ${PORT}`);
+				console.log(`App is listening on port ${PORT} (HTTP)`);
+			});
+			// Redirect from HTTP on port 80 to HTTP on port PORT
+			http.createServer((req, res) => {
+				res.writeHead(301, { "Location": `http://localhost:${PORT}${req.url}` });
+				res.end();
+			}).listen(80, () => {
+				console.log("HTTP server listening on port 80");
 			});
 		}
-
-		http.createServer((req, res) => {
-			res.writeHead(301, { "Location": `https://localhost:${PORT}${req.url}` });
-			res.end();
-		}).listen(80);
-
 	})
 	.catch((error) => {
 		console.log(error);
