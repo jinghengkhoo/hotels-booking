cfafa1ad14eb0c161171474dd37e2db87fa6e111
Roshan Samuel <flashdroid15@gmail.com>
2024-07-25 13:03:38 +0800

    cleared irrelevant comments

--- frontend/src/components/HotelSearch.test.jsx
+++ frontend/src/components/HotelSearch.test.jsx
@@ -1,90 +1,5 @@
-// // src/__tests__/HotelSearch.test.jsx
-// import React from 'react';
-// import { render, screen, waitFor } from '@testing-library/react';
-// import HotelSearch from '../components/HotelSearch';
-// import '@testing-library/jest-dom';
-// import { MemoryRouter } from 'react-router-dom';
-// import { act } from 'react';
-// import axios from 'axios';
-
-// // Mock components
-// jest.mock('./HotelList', () => () => <div data-testid="hotel-list" />);
-// jest.mock('./LoadingIcon', () => () => <div data-testid="loading-icon" />);
-// jest.mock('./NavBar', () => () => <div data-testid="nav-bar" />);
-// jest.mock('./SearchBar', () => () => <div data-testid="search-bar" />);
-
-// jest.mock('axios');
-
-// const mockLocation = {
-//   state: {
-//     destinationId: 'A6Dz',
-//     startDate: '2024-08-01',
-//     endDate: '2024-08-02',
-//     guests: 2,
-//     rooms: 1,
-//   },
-// };
-
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useLocation: () => mockLocation,
-// }));
-
-// describe('HotelSearch', () => {
-//   test('renders HotelSearch component correctly', async () => {
-//     axios.get.mockResolvedValueOnce({
-//       data: {
-//         completed: true,
-//         hotels: [
-        //   { id: '1', name: 'ROOM INN VATICAN', price: 254.17 },
-        //   { id: '2', name: 'Rome Times Hotel', price: 457.26 },
-//         ],
-//       },
-//     });
-
-//     await act(async () => {
-//       render(
-//         <MemoryRouter>
-//           <HotelSearch />
-//         </MemoryRouter>
-//       );
-//     });
-
-//     // Check if the NavBar and SearchBar components are rendered
-//     expect(screen.getByTestId('nav-bar')).toBeInTheDocument();
-//     expect(screen.getByTestId('search-bar')).toBeInTheDocument();
-
-//     // Check if the loading icon is initially displayed
-//     // expect(screen.getByTestId('loading-icon')).toBeInTheDocument();
-
-//     // Wait for the loading state to be updated
-//     // await waitFor(() => expect(screen.queryByTestId('loading-icon')).not.toBeInTheDocument());
-
-//     // Check if the HotelList component is rendered after loading
-//     expect(screen.getByTestId('hotel-list')).toBeInTheDocument();
-//   });
-
-//   test('displays an error message when fetching hotels fails', async () => {
-//     axios.get.mockRejectedValueOnce(new Error('Error fetching hotel data'));
-
-//     await act(async () => {
-//       render(
-//         <MemoryRouter>
-//           <HotelSearch />
-//         </MemoryRouter>
-//       );
-//     });
-
-//     // Wait for the loading state to be updated
-//     // await waitFor(() => expect(screen.queryByTestId('loading-icon')).not.toBeInTheDocument());
-
-//     // Check if an error message is displayed
-//     expect(screen.getByText('Error fetching hotel data:')).toBeInTheDocument();
-//   });
-// });
-
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import HotelSearch from '../components/HotelSearch';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
@@ -138,12 +53,6 @@ describe('HotelSearch', () => {
     expect(screen.getByTestId('nav-bar')).toBeInTheDocument();
     expect(screen.getByTestId('search-bar')).toBeInTheDocument();
 
-    // Check if the loading icon is initially displayed
-    // expect(screen.getByTestId('loading-icon')).toBeInTheDocument();
-
-    // Wait for the loading state to be updated
-    // await waitFor(() => expect(screen.queryByTestId('loading-icon')).not.toBeInTheDocument());
-
     // Check if the HotelList component is rendered after loading
     expect(screen.getByTestId('hotel-list')).toBeInTheDocument();
   });
@@ -158,9 +67,6 @@ describe('HotelSearch', () => {
         </MemoryRouter>
       );
     });
-
-    // Wait for the loading state to be updated
-    // await waitFor(() => expect(screen.queryByTestId('loading-icon')).not.toBeInTheDocument());
 
     // Check if an error message is displayed
     expect(screen.getByRole('alert')).toHaveTextContent('Error fetching hotel data');
