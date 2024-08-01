edb7f86f2babb23025097e922e60f72bad28d437
Marcooos05 <marcus4030@gmail.com>
2024-08-01 17:38:11 +0800

    fix: remove edit and delete button for profile booking table

--- frontend/src/components/profilepage/ProfileBookingsTable.jsx
+++ frontend/src/components/profilepage/ProfileBookingsTable.jsx
@@ -214,18 +214,6 @@ const ProfileBookingsTable = (userData) => {
                   <td className="border border-slate-700 rounded-md text-center">
                     {booking.billingAddressPostalCode}
                   </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <AiOutlineEdit
-                        className="text-2xl text-yellow-600 cursor-pointer"
-                        onClick={() => handleEditClick(booking)}
-                      />
-                      <MdOutlineDelete
-                        className="text-2xl text-red-600 cursor-pointer"
-                        onClick={() => handleDeleteClick(booking)}
-                      />
-                    </div>
-                  </td>
                 </tr>
               ))}
             </tbody>
